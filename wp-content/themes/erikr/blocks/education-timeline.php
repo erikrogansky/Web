045fc9a483448afca444b080e620f251bae99cf5
erikrogansky <roganskyerik@gmail.com>
2026-07-04 11:11:23 +0300

    style: enhance education timeline link styles with responsive design and accessibility improvements

--- wp-content/themes/erikr/blocks/education-timeline.php
+++ wp-content/themes/erikr/blocks/education-timeline.php
@@ -195,7 +195,14 @@ class Education_Timeline extends Widget_Base {
 
                                         <?php if ($link_t && $link_u): ?>
                                             <a class="education-timeline-card__header-link paragraph paragraph--small"
-                                               href="<?= esc_url($link_u) ?>"<?= $target . $rel ?>><?= esc_html($link_t) ?></a>
+                                               href="<?= esc_url($link_u) ?>" aria-label="<?= esc_attr($link_t) ?>"<?= $target . $rel ?>>
+                                                <span class="education-timeline-card__header-link-text"><?= esc_html($link_t) ?></span>
+                                                <svg class="education-timeline-card__header-link-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                    <path d="M12 8.66667V12.6667C12 13.0203 11.8595 13.3594 11.6095 13.6095C11.3594 13.8595 11.0203 14 10.6667 14H3.33333C2.97971 14 2.64057 13.8595 2.39052 13.6095C2.14048 13.3594 2 13.0203 2 12.6667V5.33333C2 4.97971 2.14048 4.64057 2.39052 4.39052C2.64057 4.14048 2.97971 4 3.33333 4H7.33333" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M10 2H14V6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M6.66667 9.33333L14 2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </svg>
+                                            </a>
                                         <?php endif; ?>
 
                                         <?php if ($title): ?>
@@ -263,7 +270,14 @@ class Education_Timeline extends Widget_Base {
 
                                         <?php if ($link_t && $link_u): ?>
                                             <a class="education-timeline-card__header-link paragraph paragraph--small"
-                                               href="<?= esc_url($link_u) ?>"<?= $target . $rel ?>><?= esc_html($link_t) ?></a>
+                                               href="<?= esc_url($link_u) ?>" aria-label="<?= esc_attr($link_t) ?>"<?= $target . $rel ?>>
+                                                <span class="education-timeline-card__header-link-text"><?= esc_html($link_t) ?></span>
+                                                <svg class="education-timeline-card__header-link-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                    <path d="M12 8.66667V12.6667C12 13.0203 11.8595 13.3594 11.6095 13.6095C11.3594 13.8595 11.0203 14 10.6667 14H3.33333C2.97971 14 2.64057 13.8595 2.39052 13.6095C2.14048 13.3594 2 13.0203 2 12.6667V5.33333C2 4.97971 2.14048 4.64057 2.39052 4.39052C2.64057 4.14048 2.97971 4 3.33333 4H7.33333" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M10 2H14V6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M6.66667 9.33333L14 2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </svg>
+                                            </a>
                                         <?php endif; ?>
 
                                         <?php if ($title): ?>
